e5081dbba8656c7fe99274bee6680f4115287ff1
mohitgupta82345-sys <mohitgupta82345@gmail.com>
2026-09-10 22:41:02 +0530

    Time: 6 ms (87.45%) | Memory: 44.2 MB (38.01%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if (s1.length() > s2.length())
+            return false;
+
+        int[] s1Freq = new int[26];
+        int[] windowFreq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+        }
+
+        int k = s1.length();
+
+        for (int i = 0; i < k; i++) {
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(s1Freq, windowFreq))
+            return true;
+
+    
+        for (int i = k; i < s2.length(); i++) {
+
+         
+            windowFreq[s2.charAt(i) - 'a']++;
+
+          
+            windowFreq[s2.charAt(i - k) - 'a']--;
+
+            if (Arrays.equals(s1Freq, windowFreq))
+                return true;
+        }
+
+        return false;
+
+    }
+}
